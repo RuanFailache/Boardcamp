e18be056ecfcf9ffa4ac09f67c66ffc309c0ace3
RuanFailache <ruanfailache@outlook.com>
2021-10-14 10:59:59 -0300

    Starting the project

---
+++ backend-boardcamp/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import pg from "pg";
+
+const connectionData = {
+  user: "bootcamp_role",
+  password: "senha_super_hiper_ultra_secreta_do_role_do_bootcamp",
+  host: "localhost",
+  port: 5432,
+  database: "boardcamp",
+};
+
+const db = new pg.Pool(connectionData);
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(4000);
